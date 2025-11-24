fadd74e6258292bac8d942e380140abc87496eec
andres garcia <andresagustingp@gmail.com>
2025-11-24 19:42:49 -0300

    actualize app.js

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const { engine } = requiere("express-handlebars");
+const path = require("path");
 const productRoutes = require("./routes/productRoutes");
 const cartRoutes = require("./routes/cartRoutes");
+const viewsrRouter = require("./routes/viewsRouter");
 
 const app = express();
+
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "views"));
+
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname,"public")));
 
 app.use("/api/products", productRoutes);
 app.use("/api/carts", cartRoutes);
+app.use("/", viewsRouter);
 
 module.exports = app;
