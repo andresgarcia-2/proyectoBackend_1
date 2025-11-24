fdc642962050d005977b3398777189a793b1a190
andres garcia <andresagustingp@gmail.com>
2025-11-24 20:27:54 -0300

    actualize productRoutes.js

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const { getProducts, getProductById, addProduct, updateProduct, deleteProduct } = require("../controllers/productController");
+const { 
+    getProducts, 
+    getProductById, 
+    addProductHTTP, 
+    updateProduct, 
+    deleteProductHTTP 
+} = require("../controllers/productController");
 
-router.get("/", getProducts);
-router.get("/:id", getProductById);
-router.post("/", addProduct);
-router.put("/:id", updateProduct);
-router.delete("/:id", deleteProduct);
+router.get("/", async (req, res) => {
+    const products = await getProducts();
+    res.json(products);
+});
+
+router.get("/:pid", getProductById);
+router.post("/", addProductHTTP);
+router.put("/:pid", updateProduct);
+router.delete("/:pid", deleteProductHTTP);
 
 module.exports = router;
