f314f27c8931b09594b42087be2110139b28d433
andres garcia <andresagustingp@gmail.com>
2025-12-23 17:39:03 -0300

    Actualize cart.js y product.js (modelos)

--- src/models/Cart.js
+++ src/models/Cart.js
@@ -1,6 +1,30 @@
+const mongoose = require('mongoose');
+
+const cartSchema = new mongoose.Schema({
+    products: [{
+        product: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Product',
+            required: true
+        },
+
+        quantity: {
+            type: Number,
+            required: true,
+            default: 1
+        }
+    }]
+    }, {
+        timestamps: true
+});
+
+module.exports = mongoose.model('Cart', cartSchema);
+
+/*
 class Cart { 
     constructor(id, products) {
         this.id = id;
         this.products = products;
     }
 }
+*/
